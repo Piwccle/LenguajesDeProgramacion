6f968ef0ae387d08e5cb95ce31654acf083c9920
Piwccle <97563768+Piwccle@users.noreply.github.com>
2024-03-04 23:15:45 +0100

    Faltan los ejecicios en donde hay que crear clases

---
+++ Haskell/src/EjerciciosExamen.hs
@@ -0,0 +1,164 @@
+module EjerciciosExamen 
+        (pertenece,
+        eliminar,
+        productoEscalar,
+        bota1,
+        zapatilla1,
+        bota2,
+        zapatilla2,
+        bota3,
+        zapatilla3,
+        productos,
+        order,
+        librerias,
+        insertarEnArbol,
+        f,
+        separarPorPosicion,
+        p
+        ) where
+
+pertenece :: (Eq a) => a -> [a] -> Bool
+pertenece elemento = foldr (\x base -> x == elemento || base) False
+
+eliminar :: (Eq a) => a -> [a] -> [a]
+eliminar elemento = foldr (\x base -> if x == elemento then base else [x]++base) []
+
+type Vector = [Int]
+
+productoEscalar :: Vector -> Vector -> Int
+productoEscalar v1 v2 = foldl (\base x -> base + x) 0 ([(fst x * snd x) | x <- (zip v1 v2)])
+
+data Botas = Botas {marcaB :: String, numeroB :: Int}
+data Zapatillas = Zapatillas {marcaZ :: String, numeroZ :: Int, deporte :: String}
+data Calzado = Bota Botas | Zapatilla Zapatillas
+
+instance Show Calzado where
+    show (Bota b) = "Bota de la marca " ++ marcaB b ++ " del numero " ++ show (numeroB b) ++ "\n"
+    show (Zapatilla z) = "Zapatilla de la marca " ++ marcaZ z ++ " del numero " ++ show (numeroZ z) ++ " para " ++ deporte z ++ "\n"
+
+instance Eq Calzado where
+    Bota b1 == Bota b2 = marcaB b1 == marcaB b2 && numeroB b1 == numeroB b2
+    Zapatilla z1 == Zapatilla z2 = marcaZ z1 == marcaZ z2 && numeroZ z1 == numeroZ z2 && deporte z1 == deporte z2 
+    Bota _ == Zapatilla _ = False
+    Zapatilla _ == Bota _ = False
+
+getMarca :: Calzado -> String
+getMarca (Bota b) = marcaB b
+getMarca (Zapatilla z) = marcaZ z
+
+instance Ord Calzado where
+    compare c1 c2
+        | c1 == c2 = EQ
+        | getMarca c1 < getMarca c2 = LT
+        | otherwise = GT
+
+--datos de ejemplo
+bota1 :: Botas
+bota1 = (Botas "Fosco" 38)
+zapatilla1 :: Zapatillas
+zapatilla1 = (Zapatillas "Adidas" 40 "Running")
+bota2 :: Botas
+bota2 = (Botas "Diesel" 42)
+zapatilla2 :: Zapatillas
+zapatilla2 = (Zapatillas "Adidas" 40 "Running")
+bota3 :: Botas
+bota3 = (Botas "Martinelli" 40)
+zapatilla3 :: Zapatillas
+zapatilla3 = (Zapatillas "Nike" 32 "Padel")
+
+productos :: [Calzado]
+productos = [(Bota bota1),(Zapatilla zapatilla1),(Bota bota2),(Zapatilla zapatilla2),(Bota bota3),(Zapatilla zapatilla3)]
+
+order :: Ord a => [a] -> [a]
+order [] = []
+order (x:xs) = order menores ++ [x] ++ order mayores
+    where menores = [y | y <- xs, y <= x]
+          mayores = [y | y <- xs, y > x]
+
+data Version = Version {major :: Int, minor :: Int}
+
+instance Show Version where
+    show v = show (major v) ++ "." ++ show (minor v)
+
+instance Eq Version where
+    v1 == v2 = major v1 == major v2 && minor v1 == minor v2
+
+instance Ord Version where
+    compare v1 v2
+        | v1 == v2 = EQ
+        | major v1 < major v2 || (major v1 == major v2 && minor v1 < minor v2) = LT
+        |otherwise = GT
+
+data Libreria = Libreria {nombre :: String, version :: Version}
+
+instance Show Libreria where
+    show lib = nombre lib ++ " " ++ show (version lib)
+
+instance Eq Libreria where
+    lib1 == lib2 = nombre lib1 == nombre lib2 && getMajor (version lib1) == getMajor (version lib2) 
+        && getMinor (version lib1) == getMinor (version lib2)
+
+instance Ord Libreria where
+    compare lib1 lib2
+        | lib1 == lib2 = EQ
+        | nombre lib1 < nombre lib2 || (nombre lib1 == nombre lib2) && version lib1 < version lib2  = LT
+        | otherwise = GT
+
+l1 :: Libreria
+l1 = (Libreria "doker" (Version 2 1))
+l2 :: Libreria
+l2 = (Libreria "doker" (Version 3 2))
+l3 :: Libreria
+l3 = (Libreria "doker" (Version 2 2))
+l4 :: Libreria
+l4 = (Libreria "mtt" (Version 3 3))
+l5 :: Libreria
+l5 = (Libreria "ansible" (Version 1 21))
+l6 :: Libreria
+l6 = (Libreria "ruby" (Version 2 0))
+l7 :: Libreria
+l7 = (Libreria "ansible" (Version 1 2))
+
+librerias :: [Libreria]
+librerias = [l1,l2,l3,l4,l5,l6,l7]
+
+getMajor :: Version -> Int
+getMajor v = major v
+
+getMinor :: Version -> Int
+getMinor v = minor v
+
+-- No puedo hacer ni el c ni el d de momento del listado 2 de examenes
+
+--ABB
+data Arbol a = AV | Rama (Arbol a) a (Arbol a) deriving Show
+
+insertarEnArbol :: (Ord a) => a -> Arbol a -> Arbol a
+insertarEnArbol r AV = (Rama AV r AV)
+insertarEnArbol n (Rama AV raiz AV)
+    | n < raiz = (Rama (Rama AV n AV) raiz AV)
+    | otherwise = (Rama AV raiz (Rama AV n AV))
+insertarEnArbol n (Rama izq raiz der)
+    | n < raiz = (Rama (insertarEnArbol n izq) raiz der)
+    | otherwise = (Rama izq raiz (insertarEnArbol n der))
+
+isVocal :: Char -> Bool
+isVocal 'a' = True
+isVocal 'e' = True
+isVocal 'i' = True
+isVocal 'o' = True
+isVocal 'u' = True
+isVocal _ = False
+
+f :: String -> (String, String)
+f = foldr (\x (vocales,consonantes) -> if isVocal x then ([x]++vocales,consonantes) else (vocales,[x]++consonantes)) ("","")
+
+separarPorPosicion :: [a] -> ([a], [a])
+separarPorPosicion list = foldr (\x (impares,pares) -> if even x then (impares,[list!!x]++pares) else ([list!!x]++impares,pares)) ([], []) [0..(length list - 1)]
+
+--Basicamente crea una lista de listas empezando por la vacia y sigue con la lista del primer numero, la lista formada por el primero
+-- y el segundo, la lista formada por el primero segundo y tercero y asi sucesivamente
+-- cualquier aplicacion es pasarle una lista cualquiera por ejemplo p [1,2,3,4] o p "asdf"
+p :: [a] -> [[a]]
+p [] = [[]]
+p (x:xs) = []:map(x:)(p xs)
---
+++ Haskell/src/Listado3Tema5.hs
@@ -0,0 +1,210 @@
+module Listado3Tema5 
+        (qksort,
+        divisiones,
+        a1,a2,a3,a4,a5,
+        listaAsociaciones,
+        listaMatriculados,
+        mostrarAlumnos,
+        mismaFecha,
+        insertarOrdenado,
+        ordenarAux,
+        ordenar,
+        qs,
+        temporada2013,
+        getGanador,
+        ordenarListaYMostrar,
+        mostrarListadoOrdenadoTorneos,
+        o,
+        insertarEnMesa,
+        insertarMesaLibre,
+        quitarMesa,
+        encontrarMesaLibre,
+        ocuparMesa
+        ) where
+
+data Fecha = Fecha {dia :: Int, mes :: Int, año :: Int}
+
+instance Show Fecha where
+    show (Fecha d m a) = show d ++ "/" ++ show m ++ "/" ++ show a
+
+instance Eq Fecha where
+    f1 == f2 = dia f1 == dia f2 && mes f1 == mes f2 && año f1 == año f2
+
+instance Ord Fecha where
+    compare x y 
+        | x == y = EQ
+        | año x < año y || (año x == año y && mes x < mes y) || (año x == año y && mes x == mes y && dia x < dia y) = LT
+        | otherwise = GT
+    
+qksort :: (Ord a) => [a] -> [a]
+qksort [] = []
+qksort (p:xs) = qksort [x | x <- xs, x < p] ++ [p] ++ qksort [x | x <- xs, x >= p]
+
+divisiones :: (Eq a, Integral a) => a -> [a] -> [Maybe a]
+divisiones n list = [if x == 0 then Nothing else Just (n`div`x) | x <- list]
+
+data Titulacion = GradoII | GradoADE_II | GradoADE deriving Show
+
+data Estudiante = Estudiante String Titulacion
+instance Show Estudiante where
+    show (Estudiante n t) = "(" ++ n ++ show t ++ ")"
+
+a1 :: Estudiante
+a1 = Estudiante "Carlos Calle" GradoADE_II
+
+a2 :: Estudiante
+a2 = Estudiante "Irene Plaza" GradoADE
+
+a3 :: Estudiante
+a3 = Estudiante "Pepe Perez" GradoII
+
+a4 :: Estudiante 
+a4 = Estudiante "Manolo Lama" GradoADE_II
+
+a5 :: Estudiante
+a5 = Estudiante "Iciar Padilla" GradoADE
+
+data ListaMatriculados = ListaMatriculados [Estudiante] deriving Show
+data ListaAsociaciones = ListaAsociaciones [Estudiante] 
+
+listaAsociaciones :: ListaAsociaciones
+listaAsociaciones = ListaAsociaciones [a1,a2]
+
+listaMatriculados :: ListaMatriculados
+listaMatriculados = ListaMatriculados [a1,a2,a3,a4,a5]
+
+instance Show ListaAsociaciones where
+    show (ListaAsociaciones l) = show l
+
+mostrarAlumnos :: (ListaMatriculados, ListaAsociaciones) -> String
+mostrarAlumnos (_, la) = show la
+
+mismaFecha :: Fecha -> Fecha -> Bool
+mismaFecha f1 f2 = dia f1 == dia f2 && mes f1 == mes f2 && año f1 == año f2
+
+insertarOrdenado :: Fecha -> [Fecha] -> [Fecha]
+insertarOrdenado f [] = [f]
+insertarOrdenado f [x] 
+    | año f > año x = [x,f]
+    | año f <= año x && mes f > mes x = [x,f]
+    | año f <= año x && mes f <= mes x && dia f > dia x = [x,f]
+    | otherwise = [f,x]
+insertarOrdenado f (x:xs) 
+    | año f < año x = [f,x] ++ xs
+    | año f == año x && mes f < mes x = [f,x] ++ xs
+    | año f == año x && mes f == mes x && dia f < dia x = [f,x] ++ xs
+    | otherwise = [x] ++ insertarOrdenado f xs
+
+ordenarAux :: [Fecha] -> [Fecha] -> [Fecha]
+ordenarAux [] r = r
+ordenarAux (x:xs) lista = ordenarAux xs (insertarOrdenado x lista)
+
+ordenar :: [Fecha] -> [Fecha]
+ordenar lista = ordenarAux lista []
+
+qs :: [Fecha] -> [Fecha]
+qs lista = qksort lista
+
+--Falta 6 porque no se crear clases aun
+
+data Torneo = Torneo {nombre :: String, finalistas :: (String, String), resultado :: ([Int], [Int])} 
+
+instance Show Torneo where
+    show t = nombre t ++ ", Ganador: " ++ getGanador t ++ ", en " ++ show (length (fst (resultado t))) ++ " sets.\n"
+
+instance Eq Torneo where
+    t1 == t2 = nombre t1 == nombre t2 && finalistas t1 == finalistas t2 && resultado t1 == resultado t2
+
+instance Ord Torneo where
+    compare t1 t2
+        | t1 == t2 = EQ
+        | nombre t1 <= nombre t2 = LT
+        | otherwise = GT
+
+getGanador :: Torneo -> String
+getGanador t 
+    | sum (fst (resultado t)) > sum (snd (resultado t)) = fst (finalistas t)
+    | sum (fst (resultado t)) < sum (snd (resultado t)) = snd (finalistas t)
+    | sum (fst (resultado t)) < sum (snd (resultado t)) && last (fst (resultado t)) > last (snd (resultado t)) = fst (finalistas t)
+    | otherwise = snd (finalistas t)
+
+temporada2013 :: [Torneo]
+temporada2013 = [(Torneo "Open de Australia" ("Novak Djokovic","Andy Murray") ([6,7,6,6], [7,6,3,2]))] 
+    ++ [(Torneo "Indian Wells" ("Juan Martín del Potro","Rafael Nadal") ([6,3,4],[4,6,6]))]
+    ++ [(Torneo "Mutua Madrid Open" ("Rafael Nadal", "Stanislas Wawrinka") ([6,6],[2,4]))]
+    ++ [(Torneo "Wimbledon" ("Novak Djokovic", "Andy Murray") ([6,7,6],[4,5,4]))]
+
+ordenarListaYMostrar :: [Torneo] -> String
+ordenarListaYMostrar [] = ""
+ordenarListaYMostrar (x:xs) = show x ++ ordenarListaYMostrar xs
+
+mostrarListadoOrdenadoTorneos :: [Torneo] -> String
+mostrarListadoOrdenadoTorneos list = ordenarListaYMostrar (qksort list)
+
+data Student = Student {fullname :: String, age :: Integer, qualifications :: [Integer]}
+
+instance Eq Student where
+    s1 == s2 = fullname s1 == fullname s2 && age s1 == age s2 && qualifications s1 == qualifications s2
+
+--Si suponemos que un estudiante es mayor que otro por la edad seria asi, si fuera
+--alfabeticamente habria que cambiar age por fullname
+instance Ord Student where
+    compare x y 
+        | x == y = EQ
+        | age x <= age y = LT
+        | otherwise = GT
+
+data Mesa = Mesa {numeroMesa :: Int, capacidad :: Int}
+
+instance Show Mesa where
+    show m = "Mesa " ++ show (numeroMesa m) ++ " -> Capacidad: " ++ show (capacidad m)
+
+instance Eq Mesa where
+    m1 == m2 = numeroMesa m1 == numeroMesa m2 && capacidad m1 == capacidad m2
+
+instance Ord Mesa where
+    compare m1 m2
+        | m1 == m2 = EQ
+        | capacidad m1 <= capacidad m2 = LT
+        | otherwise = GT
+
+data Ocupacion = Ocupacion {libres :: [Mesa], ocupadas :: [Mesa]}
+
+instance Show Ocupacion where
+    show oc = "Libres: \n" ++ show (libres oc) ++ "\nOcupadas: \n" ++ show (ocupadas oc)
+
+o :: Ocupacion
+o = Ocupacion [(Mesa 3 5), (Mesa 1 10)] [(Mesa 2 20)]
+
+insertarEnMesa :: [Mesa] -> Mesa -> [Mesa]
+insertarEnMesa [] _ = []
+insertarEnMesa [x] m 
+    | x < m = [x,m]
+    | otherwise = [m,x]
+insertarEnMesa (x:xs) m 
+    | m < x = [m,x] ++ xs
+    | otherwise = [x] ++ insertarEnMesa xs m 
+
+insertarMesaLibre :: Ocupacion -> Mesa -> Ocupacion
+insertarMesaLibre o1 m1  
+    | null (libres o1) = (Ocupacion [m1] (ocupadas o1))
+    | otherwise = (Ocupacion (insertarEnMesa (libres o1) m1) (ocupadas o1))
+
+type NumeroComensales = Int 
+
+quitarMesa :: [Mesa] -> Mesa -> [Mesa]
+quitarMesa lista m = [x | x <- lista, not(x == m)]
+
+encontrarMesaLibre :: [Mesa] -> NumeroComensales -> Mesa
+encontrarMesaLibre [] _ = error "No hay mesas libres para estos comensales"
+encontrarMesaLibre (x:xs) num
+    | capacidad x > num = x
+    | otherwise = encontrarMesaLibre xs num
+
+ocuparMesa :: Ocupacion -> NumeroComensales -> Ocupacion
+ocuparMesa o1 num = (Ocupacion (quitarMesa (libres o1) mesa) (insertarEnMesa (ocupadas o1) mesa))
+    where mesa = encontrarMesaLibre (libres o1) num
+
+--No puedo hacer el 10 hasta el miercoles
+--Ni el 11
+--Ni el 12
